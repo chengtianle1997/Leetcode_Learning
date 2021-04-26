bf29ee25a196859e8769a775c863958ed93de63f
Cheng Tianle <chengtianle1997@163.com>
2021-04-26 14:13:27 +0800

    19. Remove Nth Node From End of List

--- 19. RemoveNthNodeFromEndofList/RemoveNthNodeFromEndofList.cpp
+++ 19. RemoveNthNodeFromEndofList/RemoveNthNodeFromEndofList.cpp
@@ -18,13 +18,25 @@ public:
 		ListNode* dummyHead = new ListNode(0, head);
 		ListNode* p = dummyHead;
 		ListNode* q = dummyHead;
+		// finally, we have q -> nullptr, p -> the last n+1
 		while (n > 0 && q)
 		{
 			q = q->next;
 			n--;
 		}
+		// to keep p the last n+1 node
 		q = q->next;
-		return head;
+		while (q)
+		{
+			p = p->next;
+			q = q->next;
+		}
+		// p -> the last n+1
+		// remove the last n node behind p
+		ListNode* temp = p->next;
+		p->next = temp->next;
+		delete temp;
+		return dummyHead->next;
     }
 };
 
@@ -64,14 +76,14 @@ int main()
 	Solution solution;
 	// test case 1: empty linked list
 	ListNode* head_a = CreateList({});
-	solution.removeNthFromEnd(head_a, 1);
+	head_a = solution.removeNthFromEnd(head_a, 1);
 	PrintList(head_a);
 	// test case 2: remove the only element
 	ListNode* head_b = CreateList({1});
-	solution.removeNthFromEnd(head_b, 1);
+	head_b = solution.removeNthFromEnd(head_b, 1);
 	PrintList(head_b);
 	// test case 3: success remove
 	ListNode* head_c = CreateList({1, 2, 3, 4, 5 });
-	solution.removeNthFromEnd(head_c, 2);
+	head_c = solution.removeNthFromEnd(head_c, 2);
 	PrintList(head_c);
 }
